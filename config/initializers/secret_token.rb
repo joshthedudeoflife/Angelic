57a79ff7b5681babb8d81747f2c7ca0f9292c17e
Lee Carter <8lcarte@gmail.com>
2014-04-22 20:54:52 -0400

    First commit, foundation installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angelic::Application.config.secret_key_base = 'ad0e9635222ef1b77ecfb9329ad8d0cdd7b4860dcb54a38b9dcfea7f8649095927ca96a49a5f925d0f8d7a48c2b3cc76d8ea840bf1e4bc378cc0594c2080ff0c'
