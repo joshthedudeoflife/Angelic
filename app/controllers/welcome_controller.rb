fc4edc096e6f2fd29570bde06d3cae3f8f8adbf4
Lee Carter <8lcarte@gmail.com>
2014-05-04 21:20:47 -0400

    updated splash page and routes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+  before_action :redirect_if_signed_in
   def index
+  end
+
+private
+
+  def redirect_if_signed_in
+ 		redirect_to companies_index_path if user_signed_in?
   end
 end
